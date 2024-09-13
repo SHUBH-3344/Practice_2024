d79c3464de29375491d5d62107bd4d4095a9338d
SHUBH <shubh.java24@gmail.com>
2024-09-13 15:11:48 +0530

    springBoot app

---
+++ SB002_SpringBootApp/src/main/java/in/shubh/Sb002SpringBootAppApplication.java
@@ -0,0 +1,13 @@
+package in.shubh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sb002SpringBootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sb002SpringBootAppApplication.class, args);
+	}
+
+}
---
+++ SB002_SpringBootApp/src/test/java/in/shubh/Sb002SpringBootAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.shubh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sb002SpringBootAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
